f396003259fe646c8be1958091b899a0f6655e20
alwright33 <austinlucaswright@gmail.com>
2024-09-30 10:56:22 -0500

    login and register pages are functional without styling

---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { getUserByEmail } from "../../services/userServices";
+
+export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    getUserByEmail(email).then((foundUsers) => {
+      if (foundUsers.length === 1) {
+        const user = foundUsers[0];
+
+        if (user.password === password) {
+          localStorage.setItem(
+            "chrono_user",
+            JSON.stringify({
+              id: user.id,
+            })
+          );
+
+          navigate("/");
+        } else {
+          window.alert("Invalid password");
+        }
+      } else {
+        window.alert("Invalid login");
+      }
+    });
+  };
+
+  return (
+    <main className="auth-container">
+      <section>
+        <form className="auth-form" onSubmit={handleLogin}>
+          <h1 className="header">ChronoSync</h1>
+          <h2>Please sign in</h2>
+          <fieldset className="auth-fieldset">
+            <div>
+              <input
+                type="email"
+                value={email}
+                className="auth-form-input"
+                onChange={(evt) => setEmail(evt.target.value)}
+                placeholder="Email address"
+                required
+                autoFocus
+              />
+            </div>
+          </fieldset>
+          <fieldset className="auth-fieldset">
+            <div>
+              <input
+                type="password"
+                value={password}
+                className="auth-form-input"
+                onChange={(evt) => setPassword(evt.target.value)}
+                placeholder="Password"
+                required
+              />
+            </div>
+          </fieldset>
+          <fieldset className="auth-fieldset">
+            <div>
+              <button type="submit">Sign in</button>
+            </div>
+          </fieldset>
+        </form>
+      </section>
+      <section className="register-link">
+        Not a member? <Link to="/register">Click here to sign up.</Link>
+      </section>
+    </main>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { App } from "./App.jsx"; // Named import (or change to default if using default export)
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
